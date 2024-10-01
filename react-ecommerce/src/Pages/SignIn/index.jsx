ae3937a417a2afaf39d9b39f9d65caad0b710d8a
Cristian Florez <133178936+Criswa7@users.noreply.github.com>
2024-10-01 20:39:53 +0000

    feat: Implement basic layout structure

--- react-ecommerce/src/Pages/SignIn/index.jsx
+++ react-ecommerce/src/Pages/SignIn/index.jsx
@@ -1,8 +1,10 @@
+import Layout from '../../Components/Layout'
+
 function SingIn() {
     return (
-      <div className="bg-red-100">
+      <Layout>
         SingIn
-      </div>
+      </Layout>
     )
   }
   
